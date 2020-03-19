7f3bcba89abfcc23dac9dd1d9cac480651e4b83b
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-19 23:02:30 +0900

    Add modules.py from model.py

--- model.py
+++ model.py
@@ -1,23 +1,4 @@
 import torch
 
-class CausalBlock(torch.nn.Module):
-    pass
-
-class DiluteBlock(torch.nn.Module):
-    pass
-
-class PostProcessingBlock(torch.nn.Module):
-    pass
-
-class ConditionModule(torch.nn.Module):
-    pass
-
-class SourceModule(torch.nn.Module):
-    pass
-
-# Causal + Dilute1 + ... + DiluteN + PostProcessing
-class NeuralFilterModule(torch.nn.Module):
-    pass
-
 class NSFModel(torch.nn.Module):
     pass
---
+++ modules.py
@@ -0,0 +1,21 @@
+import torch
+
+class CausalBlock(torch.nn.Module):
+    pass
+
+class DiluteBlock(torch.nn.Module):
+    pass
+
+class PostProcessingBlock(torch.nn.Module):
+    pass
+
+class ConditionModule(torch.nn.Module):
+    pass
+
+class SourceModule(torch.nn.Module):
+    pass
+
+# Causal + Dilute1 + ... + DiluteN + PostProcessing
+class NeuralFilterModule(torch.nn.Module):
+    pass
+
