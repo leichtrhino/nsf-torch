7700050790fd8858190af418b1c88db7da3e5f39
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-19 17:03:50 +0900

    Add phase distance to losses.py

--- losses.py
+++ losses.py
@@ -20,4 +20,14 @@ def spectral_amplitude_distance(dft_bins, frame_length, frame_shift):
     return _distance
 
 def phase_distance(dft_bins, frame_length, frame_shift):
-    raise NotImplementedError()
+    def _distance(x, y):
+        X = torch.stft(x, dft_bins, frame_shift, frame_length)
+        Y = torch.stft(y, dft_bins, frame_shift, frame_length)
+        return torch.sum(
+            1 - (X[:,:,0]*Y[:,:,0] + X[:,:,1]*Y[:,:,1])
+            / torch.sqrt(torch.clamp(
+                ((X[:,:,0]**2+X[:,:,1]**2)*(Y[:,:,0]**2+Y[:,:,1]**2)),
+                min=torch.finfo(X.dtype).eps
+            ))
+        )
+    return _distance
