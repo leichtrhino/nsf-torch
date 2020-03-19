edea290465d393da8615577df8cd425611f3baa6
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-19 15:54:59 +0900

    Add spectral amplitude distance

---
+++ losses.py
@@ -0,0 +1,23 @@
+import torch
+from math import pi
+
+def spectral_amplitude_distance(dft_bins, frame_length, frame_shift):
+    def _distance(x, y):
+        X = torch.stft(x, dft_bins, frame_shift, frame_length)
+        Y = torch.stft(y, dft_bins, frame_shift, frame_length)
+        return 0.5 * torch.sum(
+            torch.log(
+                torch.clamp(
+                    X[:,:,0]**2 + X[:,:,1]**2,
+                    min=torch.finfo(X.dtype).eps
+                ) /
+                torch.clamp(
+                    Y[:,:,0]**2 + Y[:,:,1]**2,
+                    min=torch.finfo(Y.dtype).eps
+                )
+            )**2
+        )
+    return _distance
+
+def phase_distance(dft_bins, frame_length, frame_shift):
+    raise NotImplementedError()
