adb4ca1643ba760a04b00f86667ccf612f809f0e
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-19 11:57:08 +0900

    Update example of signal generator

--- layers.py
+++ layers.py
@@ -34,14 +34,34 @@ def main():
     import matplotlib.pyplot as plt
     import librosa
     import librosa.display
-    singen = SineGenerator(160000)
-    linear = torch.nn.Linear(8, 1)
     # unsqueeze to make 1x16x1 tensor
     x = torch.cat(
-        (torch.zeros(6), 220 * torch.ones(5), 440 * torch.ones(5))
+    #    (torch.zeros(6), 220 * torch.ones(5), 440 * torch.ones(5))
+        (440 * torch.ones(8000),)
     ).unsqueeze(-1).unsqueeze(0)
-    y = linear(singen(x))
-    print(x.shape, y.shape)
+    y, _ = librosa.core.load('名称未設定.wav', sr=16000)
+    waveform_length = y.size
+    y = torch.from_numpy(y).unsqueeze(-1).unsqueeze(0)
+
+    model = torch.nn.Sequential(
+        SineGenerator(waveform_length),
+        torch.nn.Linear(8, 1)
+    )
+    criterion = torch.nn.MSELoss(reduction='sum')
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+    for t in range(500):
+        y_pred = model(x)
+
+        # Compute and print loss
+        loss = criterion(y_pred, y)
+        if t % 100 == 99:
+            print(t, loss.item())
+
+        # Zero gradients, perform a backward pass, and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
     '''
     y = np.array(torch.sum(y, -1)[0]) # merge harmonics and squeeze
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
