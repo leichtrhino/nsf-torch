1a11b29e27ed38823ecb8afeb30c3289d3119d79
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-20 10:42:32 +0900

    Update test to train source module

--- test_train_sourcemodule.py
+++ test_train_sourcemodule.py
@@ -6,11 +6,8 @@ import librosa
 import librosa.display
 
 from layers import SineGenerator
+from modules import SourceModule
 from losses import spectral_amplitude_distance, phase_distance
-
-class SqueezeLayer(torch.nn.Module):
-    def forward(self, x):
-        return torch.squeeze(x, -1)
 
 def main():
     # unsqueeze to make 1x16x1 tensor
@@ -19,15 +16,13 @@ def main():
     coeff, bias = torch.randn(8), torch.randn(1)
     y = torch.sum(coeff * SineGenerator(waveform_length)(x), -1) + bias
 
-    s = SineGenerator(waveform_length)
-    l = torch.nn.Linear(8, 1)
-    model = torch.nn.Sequential(s, l, SqueezeLayer())
+    model = SourceModule(waveform_length)
     Ls = spectral_amplitude_distance(512, 320, 80)
     Lp = phase_distance(512, 320, 80)
     criterion = lambda y_pred, y: Ls(y_pred, y) + Lp(y_pred, y)
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
     for t in range(501):
-        s.natural_waveforms = y
+        model.sine_generator.natural_waveforms = y
         y_pred = model(x)
 
         # Compute and print loss
@@ -41,7 +36,7 @@ def main():
         optimizer.step()
 
     print('true parameters:', coeff, bias)
-    print('predicted parameters:', l.weight, l.bias)
+    print('predicted parameters:', model.linear.weight, model.linear.bias)
 
 if __name__ == '__main__':
     main()
