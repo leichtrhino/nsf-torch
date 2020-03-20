194c7dd013ec43731ab531ea5d12e75d6c42cb77
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-03-20 10:29:38 +0900

    Add Condition module and simple test code of the module

--- modules.py
+++ modules.py
@@ -1,5 +1,27 @@
 import torch
 
+class ConditionModule(torch.nn.Module):
+    def __init__(self, input_size, output_size):
+        super(ConditionModule, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.bilstm = torch.nn.LSTM(
+            input_size=input_size, hidden_size=32,
+            batch_first=True, bidirectional=True
+        )
+        self.cnn = torch.nn.Conv1d(
+            in_channels=64, out_channels=output_size-1, kernel_size=1
+        )
+
+    def forward(self, x):
+        F0 = x[:, :, 0].unsqueeze(dim=-1)
+        x, _ = self.bilstm(x) # NOTE: this ignores hidden states
+        x = self.cnn(x.permute(0, 2, 1)).permute(0, 2, 1)
+        return torch.cat((F0, x), dim=-1)
+
+class SourceModule(torch.nn.Module):
+    pass
+
 class CausalBlock(torch.nn.Module):
     pass
 
@@ -9,13 +31,35 @@ class DiluteBlock(torch.nn.Module):
 class PostProcessingBlock(torch.nn.Module):
     pass
 
-class ConditionModule(torch.nn.Module):
-    pass
-
-class SourceModule(torch.nn.Module):
-    pass
-
 # Causal + Dilute1 + ... + DiluteN + PostProcessing
 class NeuralFilterModule(torch.nn.Module):
     pass
 
+def main():
+    batch_size = 8
+    context_length = 80
+    feature_dim = 81
+    output_dim = 64
+
+    x = torch.randn(batch_size, context_length, feature_dim)
+    y = torch.randn(batch_size, context_length, output_dim)
+
+    module = ConditionModule(feature_dim, output_dim)
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(module.parameters(), lr=1e-4)
+
+    for t in range(501):
+        y_pred = module(x)
+
+        # Compute and print loss
+        loss = criterion(y_pred, y)
+        if t % 100 == 0:
+            print(t, loss.item())
+
+        # Zero gradients, perform a backward pass, and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+if __name__ == '__main__':
+    main()
